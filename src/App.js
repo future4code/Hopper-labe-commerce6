a50bf378cb4bdc96f518e1cf95646b6599f23132
Alison Lucas Ribeiro <alisonluri@hotmail.com>
2022-05-15 16:18:16 -0300

    Projeto não finalizado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
 import CardCamiseta from './components/CardCamiseta.js';
 //import infoProduto from '../dados/produtos.json';
 
@@ -16,11 +15,16 @@ const MainFilter = styled.div`
 display:flex;
 flex-direction: row;
 align-items: center;
+justify-content: space-around;
 `
 const MainCard = styled.div`
 display: flex;
 flex-flow: wrap;
 justify-content: space-around;
+`
+const Ordenar = styled.div`
+display: flex;
+
 `
 
 // // const ImagemCabecalho = styled.img`
@@ -38,51 +42,51 @@ class App extends React.Component {
 state ={
   dados: [
   {
-      id: Date.now(),
+      id: 1,
       imagem: "https://img.elo7.com.br/product/294x234/21C0E30/camiseta-baby-look-feminina-olho-do-universo-estampa-total-camiseta-hd.jpg",
       titulo: "Camiseta 1",
       preco: 55
   },
   {
-      id: Date.now(),
+      id: 2,
       imagem: "https://img.elo7.com.br/product/294x234/21B3412/camiseta-masculina-longline-swag-espacial-astronauta-masculina.jpg",
       titulo: "Camiseta 2",
       preco: 100
   },
 
   {
-      id: Date.now(),
+      id: 3,
       imagem: "https://img.elo7.com.br/product/294x234/21B3D1B/camiseta-masculina-longline-swag-galaxia-estampa-digital-masculina.jpg",
       titulo: "Camiseta 3",
       preco: 75 
   },
   {
-      id: Date.now(),
+      id: 4,
       imagem: "https://img.elo7.com.br/product/294x234/21B43AF/camiseta-masculina-longline-swag-naves-espaciais-full-print.jpg",
       titulo: "Camiseta 4",
       preco: 65 
   },
   {
-      id: Date.now(),
+      id: "5",
       imagem: "https://img.elo7.com.br/product/294x234/21B448C/camiseta-masculina-longline-swag-olho-do-universo-olho.jpg",
       titulo: "Camiseta 5",
       preco: 80
   },
   {
-      id: Date.now(),
+      id: 6,
       imagem: "https://img.elo7.com.br/product/294x234/2E534F1/camiseta-baby-look-feminina-galaxia-nebulosa-md04-camiseta.jpg",
       titulo: "Camiseta 6",
       preco: 170
   },
 
   {
-      id: Date.now(),
+      id: 7,
       imagem: "https://img.elo7.com.br/product/294x234/2E4DE4F/camiseta-masculina-galaxia-chuva-de-meteoros-md05-estrela.jpg",
       titulo: "Camiseta 7",
       preco: 125 
   },
   {
-      id: Date.now(),
+      id: 8,
       imagem: "https://img.elo7.com.br/product/294x234/2E4DE17/camiseta-masculina-galaxia-nebulosa-md03-total-print.jpg",
       titulo: "Camiseta 8",
       preco: 98 
@@ -90,8 +94,11 @@ state ={
 ],
   min: "",
   max: "",
-  prod: ""
+  prod: "",
+  valorCarro: "",
+  tituloCarro: ""
 }
+
 
 
  onChangeMinimo=(event)=>{
@@ -101,7 +108,7 @@ state ={
  })
  }
  
- onChangeMaximo=(event)=>{
+ onChangeMaximo=(event)=>{ 
    
   this.setState({
  max: event.target.value
@@ -115,9 +122,32 @@ state ={
     })
     }
 
+//  componentDidMount() {
+//      this.adcProdutos()
+//  }
+
+//  salvaInfos = () => {
+//      localStorage.setItem('camiseta', this.state.dados.titulo)
+//      localStorage.setItem('valor', this.state.dados.preco)
+//  }
+
+//  adcProdutos = () => {
+//      const tituloLS = localStorage.getItem('camiseta')
+//      const precoLS = localStorage.getItem('valor')
+//      const qualquer = "Trabalho"
+//  }
+
+    adcionaCarro = () => {
+        const carro = {
+            
+        }
+    }
+
+
   render () {
     const cardProduto = this.state.dados.map((card) => {
-      return <CardCamiseta 
+      return <CardCamiseta
+                key={card.id}
                 titulo={card.titulo}
                 imagem={card.imagem}
                 valor={card.preco}
@@ -130,7 +160,11 @@ state ={
 <Header />
       <MainContainer>
           <MainFilter>
-            <p>Filtro</p>
+            <input 
+                placeholder='Nome do Produto'
+                value={this.state.prod}
+                onChange={this.onChangeProduto}
+                />
             <input 
                 placeholder='Digite o Valor Mínimo'
                 value ={this.state.min}
@@ -141,22 +175,22 @@ state ={
                 value={this.state.max}
                 onChange={this.onChangeMaximo}
                 />
-            <input 
-                placeholder='Nome do Produto'
-                value={this.state.prod}
-                onChange={this.onChangeProduto}
-                />
 
-              <div>
-                  <p>Products</p>
-              </div>
-              <select>
-                <option>Crescente</option>
-                <option>Decrescente</option>
-              </select>
+            <Ordenar>
+                <p>Ordem Produtos</p>
+                <select>
+                    <option>Crescente</option>
+                    <option>Decrescente</option>
+                </select>
+            </Ordenar>
           </MainFilter>
           <MainCard>              
               {cardProduto}
+              <div>
+                <h3>Carrinho:</h3>
+                <p>{this.qualquer}</p>
+                <p>Valor Total:</p>
+              </div>
           </MainCard>      
        
       </MainContainer>
